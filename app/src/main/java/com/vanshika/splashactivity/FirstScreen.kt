ce2f5790255569ce8520408e720b7fd33d9ab4ad
Vanshika Gupta <vanshikagupta0009@gmail.com>
2024-06-26 16:48:54 +0530

    Initial commit

---
+++ app/src/main/java/com/vanshika/splashactivity/FirstScreen.kt
@@ -0,0 +1,20 @@
+package com.vanshika.splashactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class FirstScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_screen)
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+        }, 2000)
+    }
+}
